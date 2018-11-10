2b7f145fb51d7b26af43bfe9eaecd9ff17b79e86
Alejandro Salazar <alejandro.salazar@qacg.com>
2018-11-10 13:39:53 -0600

    Creación de las 3 capas del MVP.

---
+++ app/src/main/java/com/qacg/travelapp/presents/LoginPresenter.java
@@ -0,0 +1,12 @@
+package com.qacg.travelapp.presents;
+
+import com.qacg.travelapp.views.ILoginView;
+
+public class LoginPresenter {
+
+    private ILoginView view;
+
+    public LoginPresenter(ILoginView view) {
+        this.view = view;
+    }
+}
---
+++ app/src/main/java/com/qacg/travelapp/views/ILoginView.java
@@ -0,0 +1,14 @@
+package com.qacg.travelapp.views;
+
+import com.qacg.travelapp.models.User;
+
+/**
+ * Vista que define el comportamiento de la vista.
+ */
+public interface ILoginView {
+    void userFound(User user);
+
+    void userNotFound();
+
+    void emptyFields();
+}
