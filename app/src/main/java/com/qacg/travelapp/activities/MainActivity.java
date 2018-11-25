d14698cd92d5b03078d2141ab0d2923736d80b58
Gerardo Epitacio <juan.epitacio@qacg.com>
2018-11-25 10:39:47 -0600

    Close session button and screen orientation to portrait.

--- app/src/main/java/com/qacg/travelapp/activities/MainActivity.java
+++ app/src/main/java/com/qacg/travelapp/activities/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.qacg.travelapp.R;
 import com.qacg.travelapp.fragments.PlacesFragment;
@@ -17,8 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         validateSession();
-        /* the data is deleted in order to execute all flows when the app runs  */
-//        SharedPreferenceUtils.getInstance(this).clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.item_close_session) {
+            SharedPreferenceUtils.getInstance(this).clear();
+            startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     private void validateSession() {
